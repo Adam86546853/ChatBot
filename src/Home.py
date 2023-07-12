dbe4cbbc32f86e2fcdb2c065ff976430439fc50d
dlv <Adam86546853@gmail.com>
2023-07-12 10:06:10 +0800

    first update 20230712

--- src/Home.py
+++ src/Home.py
@@ -2,27 +2,23 @@ import streamlit as st
 
 
 #Config
-st.set_page_config(layout="wide", page_icon="💬", page_title="Robby | Chat-Bot 🤖")
+st.set_page_config(layout="wide", page_icon="🇨🇳", page_title="ChatBot")
 
 
 #Contact
-with st.sidebar.expander("📬 Contact"):
+with st.sidebar.expander("Contact"):
 
     st.write("**GitHub:**",
-"[yvann-hub/Robby-chatbot](https://github.com/yvann-hub/Robby-chatbot)")
+"[adam86546853/chatbot](https://github.com/Adam86546853/Robby-chatbot)")
 
-    st.write("**Medium:** "
-"[@yvann-hub](https://medium.com/@yvann-hub)")
-
-    st.write("**Twitter:** [@yvann_hub](https://twitter.com/yvann_hub)")
-    st.write("**Mail** : barbot.yvann@gmail.com")
-    st.write("**Created by Yvann**")
+    st.write("**Mail** : adam86546853@gmail.com")
+    st.write("**Created by adam86546853**")
 
 
 #Title
 st.markdown(
     """
-    <h2 style='text-align: center;'>Robby, your data-aware assistant 🤖</h1>
+    <h2 style='text-align: center;'>🇨🇳 LLM QA(智能推荐算法深圳组)</h1>
     """,
     unsafe_allow_html=True,)
 
@@ -32,29 +28,27 @@ st.markdown("---")
 #Description
 st.markdown(
     """ 
-    <h5 style='text-align:center;'>I'm Robby, an intelligent chatbot created by combining 
-    the strengths of Langchain and Streamlit. I use large language models to provide
-    context-sensitive interactions. My goal is to help you better understand your data.
-    I support PDF, TXT, CSV, Youtube transcript 🧠</h5>
+    <h5 style='text-align:left;'> 
+    LLM QA: Empowering langchain and streamlit to support PDF, TXT, CSV, and YouTube transcript!</h5>
     """,
     unsafe_allow_html=True)
 st.markdown("---")
 
 
 #Robby's Pages
-st.subheader("🚀 Robby's Pages")
+st.subheader("Discover")
 st.write("""
-- **Robby-Chat**: General Chat on data (PDF, TXT,CSV) with a [vectorstore](https://github.com/facebookresearch/faiss) (index useful parts(max 4) for respond to the user) | works with [ConversationalRetrievalChain](https://python.langchain.com/en/latest/modules/chains/index_examples/chat_vector_db.html)
-- **Robby-Sheet** (beta): Chat on tabular data (CSV) | for precise information | process the whole file | works with [CSV_Agent](https://python.langchain.com/en/latest/modules/agents/toolkits/examples/csv.html) + [PandasAI](https://github.com/gventuri/pandas-ai) for data manipulation and graph creation
-- **Robby-Youtube**: Summarize YouTube videos with [summarize-chain](https://python.langchain.com/en/latest/modules/chains/index_examples/summarize.html)
+- **Chat**: General Chat on data (PDF, TXT,CSV) with a [vectorstore](https://github.com/facebookresearch/faiss) (index useful parts(max 4) for respond to the user) | works with [ConversationalRetrievalChain](https://python.langchain.com/en/latest/modules/chains/index_examples/chat_vector_db.html)
+- **Sheet** (beta): Chat on tabular data (CSV) | for precise information | process the whole file | works with [CSV_Agent](https://python.langchain.com/en/latest/modules/agents/toolkits/examples/csv.html) + [PandasAI](https://github.com/gventuri/pandas-ai) for data manipulation and graph creation
+- **Youtube**: Summarize YouTube videos with [summarize-chain](https://python.langchain.com/en/latest/modules/chains/index_examples/summarize.html)
 """)
 st.markdown("---")
 
 
 #Contributing
-st.markdown("### 🎯 Contributing")
+st.markdown("### Contribute")
 st.markdown("""
-**Robby is under regular development. Feel free to contribute and help me make it even more data-aware!**
+- **Feel free to contribute and help me make it even more data-aware!**
 """, unsafe_allow_html=True)
 
 
